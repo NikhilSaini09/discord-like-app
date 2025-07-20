ff72ec8eed7bba6c907fe209ec50e3952768d773
NikhilSaini09 <nikhil1010saini@gmail.com>
2025-07-21 00:55:15 +0530

    server-setup

--- app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function test() {
-  return (
-    <div className="flex flex-col">
-      <p className="text-3xl font-bold text-indigo-500">
-          Hello Discord Clone
-      </p>
-    </div>
-  );
-}
