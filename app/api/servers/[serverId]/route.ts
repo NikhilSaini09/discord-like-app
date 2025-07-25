5ffb8208478d50e6115a0f0b8a5bf3c0a3915474
NikhilSaini09 <nikhil1010saini@gmail.com>
2025-07-25 13:09:35 +0530

    delete and leave server

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -3,6 +3,37 @@ import { NextResponse } from "next/server";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 
+export async function DELETE(
+    req: Request,
+    { params }: { params: Promise<{ serverId: string }> }
+) {
+    try {
+        const { serverId } = await params;
+
+        const profile = await currentProfile();
+
+        if(!profile) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        if(!serverId) {
+            return new NextResponse("Server ID Missing", { status: 400 });
+        }
+
+        const server = await db.server.delete({
+            where: {
+                id: serverId,
+                profileId: profile.id,
+            },
+        });
+
+        return NextResponse.json(server);
+    } catch (error) {
+        console.log("[SERVER_ID_DELETE]", error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
+
 export async function PATCH(
     req: Request,
     { params }: { params: Promise<{ serverId: string }> }
@@ -15,6 +46,10 @@ export async function PATCH(
 
         if(!profile) {
             return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        if(!serverId) {
+            return new NextResponse("Server ID Missing", { status: 400 });
         }
 
         const server = await db.server.update({
